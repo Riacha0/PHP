7d6f26935496f146ca87f896a34fdcf1b070f34c
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 10:47:53 -0400

    Create Tsa2_Act3Main.php

---
+++ CCS0043L TSA2/Tsa2_Act3Main.php
@@ -0,0 +1,11 @@
+<?php include("Tsa2_Act3Header.php"); ?>
+<?php include("Tsa2_Act3Menu.php"); ?>
+
+<table border="0" width="80%" align="center">
+    <tr>
+        <td colspan="2" class="welcome">
+            <h3>Welcome to My Resume</h3>
+            <p>Select a page above.</p>
+        </td>
+    </tr>
+</table>
