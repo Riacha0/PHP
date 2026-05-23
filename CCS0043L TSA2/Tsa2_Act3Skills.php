a770f613a6ebaa532a3267b8d64581b039fcc505
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 10:47:59 -0400

    Create Tsa2_Act3Skills.php

---
+++ CCS0043L TSA2/Tsa2_Act3Skills.php
@@ -0,0 +1,24 @@
+<?php include("Tsa2_Act3Header.php"); ?>
+<?php include("Tsa2_Act3Menu.php"); ?>
+
+<table border="1" width="80%" align="center">
+    <tr>
+        <td colspan="2" align="center">
+            <h3>Skills</h3>
+        </td>
+    </tr>
+    <tr>
+        <td><strong>Programming</strong></td>
+        <td>Knowledge in basic programming, HTML, CSS, JAVASCRIPT, PYTHON, C++, JAVA</td>
+    </tr>
+    <tr>
+        <td><strong>Cybersecurity</strong></td>
+        <td>Network defense, digital forensics, and ethical hacking</td>
+    </tr>
+    <tr>
+        <td><strong>Networking</strong></td>
+        <td>Router configuration and troubleshooting</td>
+    </tr>
+   
+</table>
+
