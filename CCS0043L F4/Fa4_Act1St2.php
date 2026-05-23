1df1b0640d26ee14d428ca744c5b59e150f0906d
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 02:18:44 -0400

    Create Fa4_Act1St2.php

---
+++ CCS0043L F4/Fa4_Act1St2.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>Jujutsu Kaisen</title>
+       <link rel="stylesheet" href="Fa4_Act1.css">
+</head>
+<body>
+
+<?php
+require 'Fa4_Act1Header.php';
+?>
+
+<div class="story-container">
+
+    <div class="story-text">
+        <h2>Jujutsu Kaisen</h2>
+        <p>
+Yuji Itadori lived a normal life until one day, everything changed.<br><br>
+
+While investigating a mysterious object at his school, Yuji and his friends 
+accidentally unleashed a dangerous cursed spirit.
+
+To protect everyone, Yuji made a desperate decision — he swallowed the cursed object, 
+which turned out to be a powerful finger belonging to a legendary curse named Sukuna.
+
+Instead of losing control completely, Yuji managed to contain the curse within himself, 
+something that shocked even the experienced jujutsu sorcerers.
+
+Now marked as a vessel of Sukuna, Yuji enters the world of jujutsu sorcery, 
+where he must fight curses and learn to control the dark power inside him.<br><br>
+
+With danger surrounding him and death always close, Yuji chooses to keep moving forward, 
+believing that he can use his strength to protect others.
+        </p>
+    </div>
+
+     <div class="story-image">
+        <img src="an2_1.jpg" alt="Chapter Image">
+    </div>
+
+</div>
+
+<a href="main1.php" class="back-btn">← Back to Home</a>
+
+<?php
+require 'Fa4_Footer.php';
+?>
+
+</body>
+</html>
