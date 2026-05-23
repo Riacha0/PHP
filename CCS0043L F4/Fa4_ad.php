1e74efdd056fcfcacd5d78e82ec3c2bfd0072662
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 02:19:01 -0400

    Create Fa4_ad.php

---
+++ CCS0043L F4/Fa4_ad.php
@@ -0,0 +1,22 @@
+<div class="sidebar">
+    <h3>POPULAR ONGOING ANIME</h3>
+
+    <div class="ad-container">
+
+        <div class="ad-item">
+            <img src="ad1.jpg" alt="Ad 1" class="ad-img">
+            <p>Killed Again, Mr. Detective<br>Latest: Episode 8</p>
+        </div>
+
+        <div class="ad-item">
+            <img src="ad2.jpg" alt="Ad 2" class="ad-img">
+            <p>Dr. Stone: Science Future<br>Latest: Episode 8</p>
+        </div>
+
+        <div class="ad-item">
+            <img src="ad3.jpg" alt="Ad 3" class="ad-img">
+            <p>Monster Eater<br>Latest: Episode 8</p>
+        </div>
+
+    </div>
+</div>
