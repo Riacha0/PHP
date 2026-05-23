54e37d37a56d48479be51f8e3c380a0060c1e987
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 02:18:39 -0400

    Create Fa4_Act1St5.php

---
+++ CCS0043L F4/Fa4_Act1St5.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Kagurabachi</title>
+    <link rel="stylesheet" href="Fa4_Act1.css">
+</head>
+<body>
+
+<?php
+require 'Fa4_Act1Header.php';
+?>
+
+<div class="story-container">
+
+    <div class="story-text">
+        <h2>Kagurabachi</h2>
+        <p>
+Chihiro Rokuhira was raised under the guidance of his father, a legendary swordsmith known for crafting powerful enchanted blades.<br><br>
+
+Living a peaceful life in their workshop, Chihiro spent his days learning the art of sword-making and dreaming of following in his father’s footsteps.However, tragedy struck when a group of dangerous sorcerers attacked their home, stealing the enchanted swords and leaving destruction behind. From that moment on, Chihiro’s life changed forever. Driven by grief and anger, he set out on a path of revenge, determined to retrieve the stolen blades and make those responsible pay.Armed with one of the last remaining enchanted swords, Chihiro enters a dark world filled with powerful enemies, dangerous magic, and deadly battles.<br><br>
+
+With every step he takes, he moves closer to his goal not just revenge, but justice for his father and the legacy he left behind.
+        </p>
+    </div>
+
+    <div class="story-image">
+        <img src="an5_1.jpg" alt="Chapter Image">
+    </div>
+
+</div>
+
+<a href="main1.php" class="back-btn">← Back to Home</a>
+
+<?php
+require 'Fa4_Footer.php';
+?>
+
+</body>
+</html>
