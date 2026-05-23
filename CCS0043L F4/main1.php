ccd1e8adec04d431b61d93b7fce6c79604a7d7ff
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 02:18:53 -0400

    Create main1.php

---
+++ CCS0043L F4/main1.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+    <title>MangaRead</title>
+    <link rel="stylesheet" href="Fa4_Act1.css">
+</head>
+<body>
+
+<?php
+require 'Fa4_Act1Header.php';
+?>
+
+<div class="vertical-layout">
+
+    <div class="story-column">
+        <a href="Fa4_Act1St1.php">
+            <img src="Chapter 1.jpg" alt="Chapter 1 Cover" class="cover-img">
+            <h3>Hunter X Hunter</h3>
+        </a>
+    </div>
+
+    <div class="story-column">
+        <a href="Fa4_Act1St2.php">
+            <img src="Chapter 2.jpg" alt="Chapter 2 Cover" class="cover-img">
+            <h3>Jujutsu Kaisen</h3>
+        </a>
+    </div>
+
+    <div class="story-column">
+        <a href="Fa4_Act1St3.php">
+            <img src="Chapter 3.jpg" alt="Chapter 3 Cover" class="cover-img">
+            <h3>One Piece</h3>
+        </a>
+    </div>
+
+    <div class="story-column">
+        <a href="Fa4_Act1St4.php">
+            <img src="Chapter 4.jpg" alt="Chapter 4 Cover" class="cover-img">
+            <h3>Slam Dunk</h3>
+        </a>
+    </div>
+
+    <div class="story-column">
+        <a href="Fa4_Act1St5.php">
+            <img src="Chapter 5.jpg" alt="Chapter 5 Cover" class="cover-img">
+            <h3>Kagurabachi</h3>
+        </a>
+    </div>
+
+</div>
+
+<?php include 'Fa4_Ad.php'; ?>
+
+<?php
+require 'Fa4_Footer.php';
+?>
+
+</body>
+</html>
