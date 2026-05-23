1685f5b5a18c4b1ac2d43c786ee2ca8d61367ff8
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 10:47:49 -0400

    Create Tsa2_Act3Header.php

---
+++ CCS0043L TSA2/Tsa2_Act3Header.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="Tsa2_Act3.css">
+
+<table border="1" width="80%" align="center">
+    <!-- Left column with photo -->
+    <tr>
+        <td rowspan="2" width="30%" align="center">
+            <a href="Tsa2_Act3Main.php">
+                <img src="images/pfp.jpg" width="120">
+            </a>
+        </td>
+        <!-- Top right cell: title -->
+        <td align="center">
+            <h3>Personal Information</h3>
+        </td>
+    </tr>
+
+    <!-- Second row: actual info -->
+    <tr>
+        <td>
+            <p><strong>Name:</strong> Charles Miguel T. Martin</p>
+            <p><strong>Address:</strong> 4663 Masikap St. V. Mapa Sta. Mesa, Manila</p>
+            <p><strong>Contact Number:</strong> 09362885720</p>
+            <p><strong>Email:</strong> ucanmailmigs@gmail.com</p>
+            <p><strong>Course:</strong> Cybersecurity Student</p>
+        </td>
+    </tr>
+</table>
