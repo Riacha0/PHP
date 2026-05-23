8b6b7aa25250d37c543c3041b0300a58f1976dc7
Riacha0 <ucanmailmigs@gmail.com>
2026-05-23 10:47:30 -0400

    Create Tsa2_Act3Affiliation.php

---
+++ CCS0043L TSA2/Tsa2_Act3Affiliation.php
@@ -0,0 +1,24 @@
+<?php include("Tsa2_Act3Header.php"); ?>
+<?php include("Tsa2_Act3Menu.php"); ?>
+
+<table border="1" width="80%" align="center">
+    <tr>
+        <td colspan="2" align="center">
+            <h3>Affiliation</h3>
+        </td>
+    </tr>
+    <tr>
+        <td><strong>Senior High School</strong></td>
+        <td>Member – Bioloegeast Club</td>
+    </tr>
+    <tr>
+        <td><strong>Junior High School</strong></td>
+        <td>Boy Scout – University of the East Manila Campus</td>
+    </tr>
+    <tr>
+        <td><strong>College</strong></td>
+        <td>Volunteer – PPCRV (NSTP Class)</td>
+    </tr>
+</table>
+
+
