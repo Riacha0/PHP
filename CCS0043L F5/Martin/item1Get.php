b27d5444d6ba45c363e1e2139241ff434bff5408
Riacha0 <ucanmailmigs@gmail.com>
2026-06-20 13:38:24 +0800

    Update item1Get.php

--- CCS0043L F5/Martin/item1Get.php
+++ CCS0043L F5/Martin/item1Get.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Mini Sign Up</title>
-    <!-- Bootstrap (optional, for styling) -->
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-5">
