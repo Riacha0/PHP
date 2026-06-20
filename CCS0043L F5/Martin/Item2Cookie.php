b54b6e0a298cdb63296b162cd843648f67d2da81
Riacha0 <ucanmailmigs@gmail.com>
2026-06-20 14:30:36 +0800

    Update Item2Cookie.php

--- CCS0043L F5/Martin/Item2Cookie.php
+++ CCS0043L F5/Martin/Item2Cookie.php
@@ -49,10 +49,10 @@ if (!isset($_COOKIE["lname"])) {
 <body>
 
 <div class="box">
-    <h2>Personal Information (Cookies)</h2>
+    <h2>Personal Information</h2>
 
     <?php
-    // Display cookies only if they still exist
+    
     if (isset($_COOKIE["fname"])) {
         echo "<p><strong>First Name:</strong> " . $_COOKIE["fname"] . "</p>";
     }
