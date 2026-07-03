ba8018933cad482e64bef227ea01e76dd5ac2930
Riacha0 <ucanmailmigs@gmail.com>
2026-07-03 07:35:41 -0400

    Create SA3B_Login.php

---
+++ CCS0043L SA3/SA3B_Login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SA3 Activity B - Login</title>
+
+    <link rel="stylesheet" href="SA3B_Style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h2>Login Form</h2>
+
+        <form action="" method="POST">
+
+            <label>Username</label>
+            <input type="text" name="username" required>
+
+            <label>Password</label>
+            <input type="password" name="password" required>
+
+            <input type="submit" name="login" value="Login">
+
+        </form>
+
+        <br>
+
+        <p>
+            Don't have an account?
+            <a href="SA3B_Register.php">Register Here</a>
+        </p>
+
+    </div>
+
+</body>
+
+</html>
