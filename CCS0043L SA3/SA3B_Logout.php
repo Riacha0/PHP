fb81b8296805a2ce45a5be87987a4d2d4191bc43
Riacha0 <ucanmailmigs@gmail.com>
2026-07-04 00:00:31 -0400

    Update SA3B_Logout.php

---
+++ CCS0043L SA3/SA3B_Logout.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+session_unset();
+
+session_destroy();
+
+echo "<script>
+
+        alert('Logged out successfully.');
+
+        window.location='SA3B_Login.php';
+
+      </script>";
+
+?>
