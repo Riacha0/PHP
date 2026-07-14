45ac96aba27b87fadd2973217b8845c070e3baab
Riacha0 <ucanmailmigs@gmail.com>
2026-07-14 06:40:32 -0400

    Update header.php

--- MyAdmin/includes/header.php
+++ MyAdmin/includes/header.php
@@ -4,9 +4,11 @@
 
     <meta charset="UTF-8">
 
-    <title>MyAdmin | Dashboard</title>
+    <title>MyAdmin</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <link rel="stylesheet" href="../css/admin.css">
 
