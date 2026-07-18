d67ef57e4432add10538e9d367f3db16e1130e31
Riacha0 <ucanmailmigs@gmail.com>
2026-07-17 22:54:02 -0400

    Update footer.php

--- MyAdmin/includes/footer.php
+++ MyAdmin/includes/footer.php
@@ -1,9 +1,8 @@
 <div class="text-center mt-5 text-secondary">
-
     Admin
-
 </div>
 
-</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+</body>
 </html>
