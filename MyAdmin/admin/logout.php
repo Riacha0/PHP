aa5cd911c739b067f8a694a352738a7a966396b3
Riacha0 <ucanmailmigs@gmail.com>
2026-07-17 23:18:46 -0400

    Update logout.php

--- MyAdmin/admin/logout.php
+++ MyAdmin/admin/logout.php
@@ -5,6 +5,8 @@ session_start();
 include("../includes/db_connection.php");
 include("../includes/functions.php");
 
+addAuditLog($conn, $_SESSION['admin'], "Logged Out");
+
 if (isset($_SESSION['admin'])) {
     addAuditLog($conn, $_SESSION['admin'], "Logged Out");
 }
